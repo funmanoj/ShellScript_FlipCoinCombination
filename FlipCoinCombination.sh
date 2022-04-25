3a94b053294a9c8b532ece8a577f817b73c5a2e2
Manoj Funde <funde.manoj@gmail.com>
2022-04-25 09:59:26 +0530

    Second use case

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,34 @@
 #! /bin/bash -x
 
-toss=$((RANDOM%2))
-if((toss ==0 ))
-then
-   echo "Heads"
-else
-   echo "Tails"
-fi
+read -p "Enter the number of times to flip a combination:" no_of_flips
+declare -A singlet_frequency=(
+
+			    [H]=0
+			    [T]=0
+
+                             )
+echo
+echo " .......................Singlet Combination........................."
+for(( flip=1; flip<=no_of_flips; flip++ ))
+do
+   echo -n "Flip-$flip is"
+   (( toss=RANDOM%2 ))
+   case $toss in
+	0)
+	   echo "Heads"
+	   (( singlet_frequency[H]++))
+	   ;;
+	1)
+	   echo "Tails"
+	   (( singlet_frequency[T]++))
+   esac
+done
+
+for combination in ${!siglet_frequency[@]}
+do
+   percentage=$(( ${singlet_frequency[$combination]}*100/no_of_flips ))
+   singlet_frequency[$combination]=$percentage
+   echo "percentage of $combination is ${singlet_frequency[$combination]}%"
+done
+
 
